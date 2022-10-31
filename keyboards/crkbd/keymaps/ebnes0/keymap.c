5d782648af9cedd29130dbd0f3ae090ed438593a
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2022-10-31 10:02:27 +0900

    update

--- keyboards/crkbd/keymaps/ebnes0/keymap.c
+++ keyboards/crkbd/keymaps/ebnes0/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 	[_RAISE] = LAYOUT_split_3x6_3(
-	KC_NO, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5,              KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
-	KC_NO, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0,              KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
-	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+	KC_TAB, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5,              KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
+	KC_LCTL, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0,              KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
+	KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
 	        KC_LALT, KC_LGUI, MO(_ADJUST),                  KC_TRNS, KC_LGUI, KC_RALT
   ),
 
